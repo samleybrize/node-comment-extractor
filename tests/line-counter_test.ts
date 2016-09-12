85ef2c8cc9c6429f463c2868a6ac52f4b2fc76d2
Stephen Berquet <stephen.berquet@gmail.com>
2016-09-13 01:13:07 +0200

    line counter tests

--- tests/line-counter_test.ts
+++ tests/line-counter_test.ts
@@ -3,5 +3,31 @@ import { expect } from 'chai';
 import { LineCounter } from '../src';
 
 describe('line counter', () => {
-    it('todo');
+    it('should detect CR end line', () => {
+        let lineCounter = new LineCounter();
+        lineCounter.addText('az\rer');
+        lineCounter.addText('\rty');
+        expect(lineCounter.getCurrentLineNumber()).to.equal(3);
+    });
+    it('should detect LF end line', () => {
+        let lineCounter = new LineCounter();
+        lineCounter.addText('az\ner');
+        lineCounter.addText('\nty');
+        lineCounter.addText('\n');
+        expect(lineCounter.getCurrentLineNumber()).to.equal(4);
+    });
+    it('should detect CRLF end line', () => {
+        let lineCounter = new LineCounter();
+        lineCounter.addText('az\r');
+        lineCounter.addText('\ner');
+        lineCounter.addText('\r\nty');
+        expect(lineCounter.getCurrentLineNumber()).to.equal(3);
+    });
+    it('should reset its state', () => {
+        let lineCounter = new LineCounter();
+        lineCounter.addText('az\ner');
+        lineCounter.reset();
+        lineCounter.addText('\rty');
+        expect(lineCounter.getCurrentLineNumber()).to.equal(2);
+    });
 });
