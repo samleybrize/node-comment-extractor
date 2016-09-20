8f170f33a210567195a2f7234bc46e1353be0e72
Stephen Berquet <stephen.berquet@gmail.com>
2016-09-20 07:36:08 +0200

    comment retriever php test

--- tests/comment-retriever/php_test.ts
+++ tests/comment-retriever/php_test.ts
@@ -17,45 +17,46 @@ describe('comment retriever: php', () => {
         let sourceCodeContent   = fs.readFileSync(fixtureFilePath, 'utf8');
         let sourceCode          = new SourceCodeString('php-sample', sourceCodeContent);
         let commentRetriever    = new CommentRetrieverPhp();
-        let commentList         = commentRetriever.getCommentList(sourceCode);
 
-        expect(commentList).to.be.an('array').that.have.lengthOf(9);
+        return commentRetriever.getCommentList(sourceCode).then((commentList) => {
+            expect(commentList).to.be.an('array').that.have.lengthOf(9);
 
-        expect(commentList[0].text).to.equal('should appear1');
-        expect(commentList[0].lineStart).to.equal(3);
-        expect(commentList[0].sourceIdentifier).to.equal('php-sample');
+            expect(commentList[0].text).to.equal('should appear1');
+            expect(commentList[0].lineStart).to.equal(3);
+            expect(commentList[0].sourceIdentifier).to.equal('php-sample');
 
-        expect(commentList[1].text).to.equal('should appear2');
-        expect(commentList[1].lineStart).to.equal(5);
-        expect(commentList[1].sourceIdentifier).to.equal('php-sample');
+            expect(commentList[1].text).to.equal('should appear2');
+            expect(commentList[1].lineStart).to.equal(5);
+            expect(commentList[1].sourceIdentifier).to.equal('php-sample');
 
-        expect(commentList[2].text).to.equal('text\nshould appear3\ntext');
-        expect(commentList[2].lineStart).to.equal(8);
-        expect(commentList[2].sourceIdentifier).to.equal('php-sample');
+            expect(commentList[2].text).to.equal('text\nshould appear3\ntext');
+            expect(commentList[2].lineStart).to.equal(8);
+            expect(commentList[2].sourceIdentifier).to.equal('php-sample');
 
-        expect(commentList[3].text).to.equal('should appear8');
-        expect(commentList[3].lineStart).to.equal(14);
-        expect(commentList[3].sourceIdentifier).to.equal('php-sample');
+            expect(commentList[3].text).to.equal('should appear8');
+            expect(commentList[3].lineStart).to.equal(14);
+            expect(commentList[3].sourceIdentifier).to.equal('php-sample');
 
-        expect(commentList[4].text).to.equal('should appear4');
-        expect(commentList[4].lineStart).to.equal(17);
-        expect(commentList[4].sourceIdentifier).to.equal('php-sample');
+            expect(commentList[4].text).to.equal('should appear4');
+            expect(commentList[4].lineStart).to.equal(17);
+            expect(commentList[4].sourceIdentifier).to.equal('php-sample');
 
-        expect(commentList[5].text).to.equal('should appear5');
-        expect(commentList[5].lineStart).to.equal(19);
-        expect(commentList[5].sourceIdentifier).to.equal('php-sample');
+            expect(commentList[5].text).to.equal('should appear5');
+            expect(commentList[5].lineStart).to.equal(19);
+            expect(commentList[5].sourceIdentifier).to.equal('php-sample');
 
-        expect(commentList[6].text).to.equal('should appear6');
-        expect(commentList[6].lineStart).to.equal(21);
-        expect(commentList[6].sourceIdentifier).to.equal('php-sample');
+            expect(commentList[6].text).to.equal('should appear6');
+            expect(commentList[6].lineStart).to.equal(21);
+            expect(commentList[6].sourceIdentifier).to.equal('php-sample');
 
-        expect(commentList[7].text).to.equal('should appear7');
-        expect(commentList[7].lineStart).to.equal(23);
-        expect(commentList[7].sourceIdentifier).to.equal('php-sample');
+            expect(commentList[7].text).to.equal('should appear7');
+            expect(commentList[7].lineStart).to.equal(23);
+            expect(commentList[7].sourceIdentifier).to.equal('php-sample');
 
-        expect(commentList[8].text).to.equal("should 'appear9'");
-        expect(commentList[8].lineStart).to.equal(70);
-        expect(commentList[8].sourceIdentifier).to.equal('php-sample');
+            expect(commentList[8].text).to.equal("should 'appear9'");
+            expect(commentList[8].lineStart).to.equal(70);
+            expect(commentList[8].sourceIdentifier).to.equal('php-sample');
+        });
     });
 
     it('should be able to handle multiple files', () => {
@@ -64,42 +65,47 @@ describe('comment retriever: php', () => {
         let sourceCode1         = new SourceCodeString('php-sample1', sourceCodeContent);
         let sourceCode2         = new SourceCodeString('php-sample2', sourceCodeContent);
         let commentRetriever    = new CommentRetrieverPhp();
-        let commentList1        = commentRetriever.getCommentList(sourceCode1);
-        let commentList2        = commentRetriever.getCommentList(sourceCode2);
 
-        expect(commentList1).to.be.an('array').that.have.lengthOf(9);
-        expect(commentList2).to.be.an('array').that.have.lengthOf(9);
+        return commentRetriever.getCommentList(sourceCode1)
+            .then((commentList) => {
+                expect(commentList).to.be.an('array').that.have.lengthOf(9);
+                expect(commentList[0].lineStart).to.equal(3);
+                expect(commentList[1].lineStart).to.equal(5);
+                expect(commentList[2].lineStart).to.equal(8);
+                expect(commentList[3].lineStart).to.equal(14);
+                expect(commentList[4].lineStart).to.equal(17);
+                expect(commentList[5].lineStart).to.equal(19);
+                expect(commentList[6].lineStart).to.equal(21);
+                expect(commentList[7].lineStart).to.equal(23);
+                expect(commentList[8].lineStart).to.equal(70);
 
-        expect(commentList1[0].lineStart).to.equal(3);
-        expect(commentList1[1].lineStart).to.equal(5);
-        expect(commentList1[2].lineStart).to.equal(8);
-        expect(commentList1[3].lineStart).to.equal(14);
-        expect(commentList1[4].lineStart).to.equal(17);
-        expect(commentList1[5].lineStart).to.equal(19);
-        expect(commentList1[6].lineStart).to.equal(21);
-        expect(commentList1[7].lineStart).to.equal(23);
-        expect(commentList1[8].lineStart).to.equal(70);
-
-        expect(commentList2[0].lineStart).to.equal(3);
-        expect(commentList2[1].lineStart).to.equal(5);
-        expect(commentList2[2].lineStart).to.equal(8);
-        expect(commentList2[3].lineStart).to.equal(14);
-        expect(commentList2[4].lineStart).to.equal(17);
-        expect(commentList2[5].lineStart).to.equal(19);
-        expect(commentList2[6].lineStart).to.equal(21);
-        expect(commentList2[7].lineStart).to.equal(23);
-        expect(commentList2[8].lineStart).to.equal(70);
+                return commentRetriever.getCommentList(sourceCode2);
+            })
+            .then((commentList) => {
+                expect(commentList).to.be.an('array').that.have.lengthOf(9);
+                expect(commentList[0].lineStart).to.equal(3);
+                expect(commentList[1].lineStart).to.equal(5);
+                expect(commentList[2].lineStart).to.equal(8);
+                expect(commentList[3].lineStart).to.equal(14);
+                expect(commentList[4].lineStart).to.equal(17);
+                expect(commentList[5].lineStart).to.equal(19);
+                expect(commentList[6].lineStart).to.equal(21);
+                expect(commentList[7].lineStart).to.equal(23);
+                expect(commentList[8].lineStart).to.equal(70);
+            })
+        ;
     });
 
     it('should add a non-ended comment when reached the end of the source code', () => {
         let sourceCodeContent   = '<?php\n// comment';
         let sourceCode          = new SourceCodeString('php-sample', sourceCodeContent);
         let commentRetriever    = new CommentRetrieverPhp();
-        let commentList         = commentRetriever.getCommentList(sourceCode);
 
-        expect(commentList).to.be.an('array').that.have.lengthOf(1);
-        expect(commentList[0].text).to.equal('comment');
-        expect(commentList[0].lineStart).to.equal(2);
-        expect(commentList[0].sourceIdentifier).to.equal('php-sample');
+        return commentRetriever.getCommentList(sourceCode).then((commentList) => {
+            expect(commentList).to.be.an('array').that.have.lengthOf(1);
+            expect(commentList[0].text).to.equal('comment');
+            expect(commentList[0].lineStart).to.equal(2);
+            expect(commentList[0].sourceIdentifier).to.equal('php-sample');
+        });
     });
 });
