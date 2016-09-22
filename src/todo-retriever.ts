dfb20377a01c16ba7d31add741ef32cfbd5bcb95
Stephen Berquet <stephen.berquet@gmail.com>
2016-09-22 17:36:30 +0200

    todo retriever

--- src/todo-retriever.ts
+++ src/todo-retriever.ts
@@ -9,7 +9,25 @@ import { Comment } from './comment';
 import { Todo } from './todo';
 
 export class TodoRetriever {
+    private tagList:string[] = ['TODO', '@TODO', '@todo', 'FIXME'];
+
+    constructor(tagList?:string[]) {
+        if (tagList) {
+            this.validateTagList(tagList);
+        }
+
+        this.tagList = tagList;
+    }
+
+    private validateTagList(tagList:string[]) {
+        for (let tag of tagList) {
+            if (tag.match(/\s/g)) {
+                throw 'White spaces are not allowed';
+            }
+        }
+    }
+
     getTodoList(commentList:Comment[]): Todo[] {
-        return;
+        return [];
     }
 }
--- tests/todo-retriever_test.ts
+++ tests/todo-retriever_test.ts
@@ -27,27 +27,53 @@ describe('todo retriever', () => {
         expect(todoList[0].line).to.equal(3);
         expect(todoList[0].sourceIdentifier).to.equal('id');
 
-        expect(todoList[0].text).to.equal('@TODO text 2');
-        expect(todoList[0].line).to.equal(5);
-        expect(todoList[0].sourceIdentifier).to.equal('id2');
+        expect(todoList[1].text).to.equal('@TODO text 2');
+        expect(todoList[1].line).to.equal(5);
+        expect(todoList[1].sourceIdentifier).to.equal('id2');
 
-        expect(todoList[0].text).to.equal('@todo text 3');
-        expect(todoList[0].line).to.equal(13);
-        expect(todoList[0].sourceIdentifier).to.equal('id3');
+        expect(todoList[2].text).to.equal('@todo text 3');
+        expect(todoList[2].line).to.equal(13);
+        expect(todoList[2].sourceIdentifier).to.equal('id3');
 
-        expect(todoList[0].text).to.equal('TODO text 4');
-        expect(todoList[0].line).to.equal(125);
-        expect(todoList[0].sourceIdentifier).to.equal('id4');
+        expect(todoList[3].text).to.equal('TODO text 4');
+        expect(todoList[3].line).to.equal(125);
+        expect(todoList[3].sourceIdentifier).to.equal('id4');
 
-        expect(todoList[0].text).to.equal('FIXME text 5');
-        expect(todoList[0].line).to.equal(126);
-        expect(todoList[0].sourceIdentifier).to.equal('id4');
+        expect(todoList[4].text).to.equal('FIXME text 5');
+        expect(todoList[4].line).to.equal(126);
+        expect(todoList[4].sourceIdentifier).to.equal('id4');
 
-        expect(todoList[0].text).to.equal('TODO text 6 TODO text 7');
-        expect(todoList[0].line).to.equal(146);
-        expect(todoList[0].sourceIdentifier).to.equal('id4');
+        expect(todoList[5].text).to.equal('TODO text 6 TODO text 7');
+        expect(todoList[5].line).to.equal(146);
+        expect(todoList[5].sourceIdentifier).to.equal('id4');
     });
 
-    it.skip('should interpret user defined tags', () => {
+    it('should interpret user defined tags', () => {
+        let commentList = [
+            new Comment('text before custom_tag text 1', 6, 'id1'),
+            new Comment('text before\n OTHER-TAG text 2\r\ntext after', 9, 'id2'),
+            new Comment('TODO should not appear', 56, 'id3'),
+        ];
+        let todoRetriever   = new TodoRetriever(['custom_tag', 'OTHER-TAG']);
+        let todoList        = todoRetriever.getTodoList(commentList);
+
+        expect(todoList).to.be.an('array').that.have.lengthOf(2);
+
+        expect(todoList[0].text).to.equal('custom_tag text 1');
+        expect(todoList[0].line).to.equal(6);
+        expect(todoList[0].sourceIdentifier).to.equal('id1');
+
+        expect(todoList[1].text).to.equal('OTHER-TAG text 2');
+        expect(todoList[1].line).to.equal(10);
+        expect(todoList[1].sourceIdentifier).to.equal('id2');
+    });
+
+    it('should throw an error when an invalid tag is detected', () => {
+        let fn = () => {
+            /* tslint:disable */
+            new TodoRetriever(['valid_tag', 'invalid tag']);
+            /* tslint:enable */
+        };
+        expect(fn).to.throw('White spaces are not allowed');
     });
 });
