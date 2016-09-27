fd2658b39558731de3e819a3a70514150c8614ea
Stephen Berquet <stephen.berquet@gmail.com>
2016-09-27 23:43:53 +0200

    html support

--- src/comment-retriever/html.ts
+++ src/comment-retriever/html.ts
@@ -7,10 +7,17 @@
 
 import { Comment } from '../comment';
 import { CommentRetriever } from './comment-retriever';
+import { ParserHelper } from './parser-helper/parser-helper';
+import { ParserHelperDeadZoneXmlTag } from './parser-helper/dead-zone/xml-tag';
+import { ParserHelperCommentMultiLineXml } from './parser-helper/comment/multi-line-xml';
 import { SourceCode } from '../source-code/source-code';
 
 export class CommentRetrieverHtml implements CommentRetriever {
     getCommentList(sourceCode:SourceCode): Promise<Comment[]> {
-        return null;
+        let parserHelperDeadZone    = new ParserHelperDeadZoneXmlTag();
+        let parserHelperComment     = new ParserHelperCommentMultiLineXml();
+        let parserHelper            = new ParserHelper(sourceCode, parserHelperDeadZone, parserHelperComment);
+
+        return parserHelper.getCommentList();
     }
 }
