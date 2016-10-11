0f1a40d45e38101c6808c4a88496c6da62da533e
Stephen Berquet <stephen.berquet@gmail.com>
2016-10-11 17:45:58 +0200

    html parser

--- src/comment-retriever/html.ts
+++ src/comment-retriever/html.ts
@@ -13,24 +13,55 @@ import { ParserHelperDeadZoneCollection } from './parser-helper/dead-zone/dead-z
 import { ParserHelperDeadZoneIgnoredZone } from './parser-helper/dead-zone/ignored-zone';
 import { ParserHelperDeadZoneXmlTag } from './parser-helper/dead-zone/xml-tag';
 import { ParserHelperCommentMultiLineXml } from './parser-helper/comment/multi-line-xml';
+import { ContextDetectorHtml, SourceCodeLanguageZoneList } from './parser-helper/context-detector/html';
 import { SourceCode } from '../source-code/source-code';
 import { SourceCodeZone } from '../source-code/zone';
 
 export class CommentRetrieverHtml implements CommentRetriever {
+    private commentRetrieverFactory:CommentRetrieverFactory;
+
     getCommentList(sourceCode:SourceCode, ignoredZoneList?:SourceCodeZone[]): Promise<Comment[]> {
         let parserHelperDeadZone    = new ParserHelperDeadZoneCollection();
         parserHelperDeadZone.addParserHelper(new ParserHelperDeadZoneXmlTag());
         let parserHelperComment     = new ParserHelperCommentMultiLineXml();
-        let parserHelper            = new ParserHelper(sourceCode, parserHelperDeadZone, parserHelperComment);
+        let contextDetector         = new ContextDetectorHtml();
+        let parserHelper            = new ParserHelper(sourceCode, parserHelperDeadZone, parserHelperComment, contextDetector);
 
         if (ignoredZoneList) {
             parserHelperDeadZone.addParserHelper(new ParserHelperDeadZoneIgnoredZone(ignoredZoneList));
         }
 
-        return parserHelper.getCommentList();
+        return parserHelper.getCommentList()
+            .then((commentList): Promise<Comment[]> => {
+                return this.t(sourceCode, contextDetector.getLanguageZoneList(), ignoredZoneList);
+            })
+        ;
+    }
+
+    private t(sourceCode:SourceCode, languageZoneList:SourceCodeLanguageZoneList[], ignoredZoneList:SourceCodeZone[]): Promise<Comment[]> {
+        for (let languageZone of languageZoneList) {
+            console.log(languageZone.languageName);
+            for (let zoneToProcess of languageZone.zoneList) {
+                sourceCode.rewind();
+                console.log(zoneToProcess);
+                // promise.then(() => {
+
+                // });
+            }
+        }
+
+        return Promise.resolve([]);
     }
 
     setCommentRetrieverFactory(commentRetrieverFactory:CommentRetrieverFactory) {
-        //
+        this.commentRetrieverFactory = commentRetrieverFactory;
+    }
+
+    private getCommentRetrieverFactory(): CommentRetrieverFactory {
+        if (!this.commentRetrieverFactory) {
+            this.commentRetrieverFactory = new CommentRetrieverFactory();
+        }
+
+        return this.commentRetrieverFactory;
     }
 }
