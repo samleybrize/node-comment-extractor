55385a80168e1c62009fe04e9f00070fc6807796
Stephen Berquet <stephen.berquet@gmail.com>
2016-10-12 08:00:38 +0200

    html comment retriever

--- src/comment-retriever/html.ts
+++ src/comment-retriever/html.ts
@@ -13,7 +13,7 @@ import { ParserHelperDeadZoneCollection } from './parser-helper/dead-zone/dead-z
 import { ParserHelperDeadZoneIgnoredZone } from './parser-helper/dead-zone/ignored-zone';
 import { ParserHelperDeadZoneXmlTag } from './parser-helper/dead-zone/xml-tag';
 import { ParserHelperCommentMultiLineXml } from './parser-helper/comment/multi-line-xml';
-import { ContextDetectorHtml, SourceCodeLanguageZoneList } from './parser-helper/context-detector/html';
+import { ContextDetectorHtml, SourceCodeLanguageZone } from './parser-helper/context-detector/html';
 import { SourceCode } from '../source-code/source-code';
 import { SourceCodeZone } from '../source-code/zone';
 
@@ -33,24 +33,32 @@ export class CommentRetrieverHtml implements CommentRetriever {
 
         return parserHelper.getCommentList()
             .then((commentList): Promise<Comment[]> => {
-                return this.t(sourceCode, contextDetector.getLanguageZoneList(), ignoredZoneList);
+                return this.processLanguageZoneList(sourceCode, contextDetector.getLanguageZoneList(), commentList);
             })
         ;
     }
 
-    private t(sourceCode:SourceCode, languageZoneList:SourceCodeLanguageZoneList[], ignoredZoneList:SourceCodeZone[]): Promise<Comment[]> {
-        for (let languageZone of languageZoneList) {
-            console.log(languageZone.languageName);
-            for (let zoneToProcess of languageZone.zoneList) {
-                sourceCode.rewind();
-                console.log(zoneToProcess);
-                // promise.then(() => {
-
-                // });
-            }
+    private processLanguageZoneList(sourceCode:SourceCode, languageZoneList:SourceCodeLanguageZone[], commentList:Comment[]): Promise<Comment[]> {
+        if (0 == languageZoneList.length) {
+            return Promise.resolve(commentList);
         }
 
-        return Promise.resolve([]);
+        let languageZone = languageZoneList.shift();
+        let parserHelper;
+
+        try {
+            parserHelper = this.getCommentRetrieverFactory().getNewCommentRetriever(languageZone.languageName);
+        } catch (error) {
+            return this.processLanguageZoneList(sourceCode, languageZoneList, commentList);
+        }
+
+        sourceCode.rewind();
+        return parserHelper.getCommentList(sourceCode)
+            .then((languageZoneCommentList) => {
+                // TODO merge comment list
+                return this.processLanguageZoneList(sourceCode, languageZoneList, commentList);
+            })
+        ;
     }
 
     setCommentRetrieverFactory(commentRetrieverFactory:CommentRetrieverFactory) {
